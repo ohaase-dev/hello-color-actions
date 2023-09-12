174c8524ec55166c106db06721451ecfefb7ee18
Olaf Haase <olaf.haase@bob.ch>
2023-09-12 13:47:10 +0000

    changed hello message

--- hello.js
+++ hello.js
@@ -1,3 +1,3 @@
-const hello = () => "Hello World from the Office Hours in the terminal!";
+const hello = () => "Hello World from Switzerland, Olaf Haase!";
 console.log(hello());
 exports.hello = hello;
